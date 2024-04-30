4dfaa3bf3b9e30cdd5df8df04061bcc762a4a83b
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-05-01 02:06:07 +0500

    ref

--- src/components/Paginate/Paginate.styles.js
+++ src/components/Paginate/Paginate.styles.js
@@ -7,7 +7,7 @@ export const PaginateBlock = styled.div`
 `;
 
 export const PaginateButtons = styled.div`
-    margin-botton: 30px;
+    margin-bottom: 30px;
     display: flex;
     flex-direction: row;
     gap: 20px;
