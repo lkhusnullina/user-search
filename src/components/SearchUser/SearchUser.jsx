50f3b2924cd9ae3ee1b42f5ec73503b76d682024
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-05-01 19:08:51 +0500

    refactoring

--- src/components/SearchUser/SearchUser.jsx
+++ src/components/SearchUser/SearchUser.jsx
@@ -9,23 +9,36 @@ function SearchUser() {
     const page = useSelector((state) => state.users.page);
     const sort = useSelector((state) => state.users.sort);
     const [username, setUsername] = useState('');
-    const [errorName, setErrorName] = useState(null);
+    const [validationError, setValidationError] = useState(null);
     const [trigger, { data: users, isLoading, error }] = useLazyGetUsersQuery({
         username,
         page,
         sort,
     });
+    const [errorStatus, setErrorStatus] = useState('');
+
+    useEffect(() => {
+        if (error) {
+            if (error.status === 403) {
+                setErrorStatus('Превышен лимит, попробуй позже');
+            } else {
+                setErrorStatus('Произошла ошибка. Попробуйте позже.');
+            }
+        }
+    }, [error]);
 
     function isValidUsername(username) {
         return /^[a-zA-Z0-9-._]*$/.test(username);
     }
 
     const handleChange = (event) => {
-        setErrorName(event.target.value);
+        setValidationError(event.target.value);
         if (!isValidUsername(event.target.value)) {
-            setErrorName('Логин пользователя может состоять только из латинских букв, цифр, дефисов');
+            setValidationError(
+                'Логин пользователя может состоять только из латинских букв, цифр, дефисов',
+            );
         } else {
-            setErrorName(null);
+            setValidationError(null);
         }
     };
 
@@ -64,13 +77,15 @@ function SearchUser() {
                 />
                 <S.SearchButton
                     onClick={searchClick}
-                    disabled={isLoading || username === '' || errorName}
+                    disabled={isLoading || username === '' || validationError}
                 >
                     {isLoading ? 'Поиск...' : 'Найти'}
                 </S.SearchButton>
             </S.SearchForm>
-            {errorName && <h4 style={{ color: 'red' }}>{errorName}</h4>}
-            {error && <span style={{ color: 'red' }}>{error.code}</span>}
+            {validationError && (
+                <h4 style={{ color: 'red' }}>{validationError}</h4>
+            )}
+            {error && <span style={{ color: 'red' }}>{errorStatus}</span>}
         </S.SearchBlock>
     );
 }
